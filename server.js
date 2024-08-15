ac6957d66e23a135353861474b1945323b19457c
OxerProjects <oxerprojects@gmail.com>
2024-08-15 12:48:59 +0300

    2.02

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const indexRouter = require('./routes/index')
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
+app.set('view options', { layout: false });
 app.use(expressLayouts)
 app.use(express.static('public'))
 
